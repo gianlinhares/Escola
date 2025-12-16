b97530fafce435127474e5e5b5d6f64bf2f7a60f
Gianluca Linhares <gian.linhares@hotmail.com>
2025-12-16 17:13:36 -0300

    Criada função estudantes

--- escola/views.py
+++ escola/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def estudantes(request):
+    if request.method == "GET":
+        estudante = {"id": 1, 
+                     "nome": "Gianluca Linhares",}
+    return JsonResponse(estudante)
